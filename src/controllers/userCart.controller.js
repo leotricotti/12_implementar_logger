fdb431da4475e0f723b2b0b4ad68d35c128bb954
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-30 11:27:36 -0300

    commit

--- src/controllers/userCart.controller.js
+++ src/controllers/userCart.controller.js
@@ -3,7 +3,6 @@ import { usersService } from "../repository/index.js";
 //Ruta que agrega el id del carrito al usuario
 async function addCartIDToUser(req, res) {
   const { cartId, email } = req.body;
-  console.log("cartId", cartId, "email", email);
   try {
     const user = await usersService.getOneUser(email);
     const userId = user[0]._id;
